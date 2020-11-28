a1ec66e9d75727cbff214e5ac89edbcb3bcef93b
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-11-27 20:34:17 -0800

    added models and started tables

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-	content = models.TextFiels(null=True, blank=True)
-	username = models.CharField(max_length=20, min_length=3)
+	content = models.TextField(null=True, blank=True)
+	username = models.CharField(max_length=20)
 	userEmail = models.EmailField()
-	
--- recipes/models.py
+++ recipes/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Recipes(models.Model):
-	# id = models.AutoField()
 	title = models.CharField(max_length=220)
-	ingredients = models.CharField(max_length=220)
-	directions = models.CharField(max_length=220)
+	ingredients = models.TextField(null=True, blank=True)
+	directions = models.TextField(null=True, blank=True)
 	content = models.TextField(null=True, blank=True)
